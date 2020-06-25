8d65cc2f7aa354bdc97b41c46f191ebef98b7b13
kpfander <kpfander@larochellegc.com>
2020-06-25 12:19:11 -0400

    changes

--- SnowFlake/CreateTablesForIngest.sql
+++ SnowFlake/CreateTablesForIngest.sql
@@ -1,22 +1,60 @@
 
 USE WAREHOUSE COMPUTE_WH;
-USE DATABASE CITIBIKE;
+USE DATABASE CITYBIKE;
 
 
-DROP TABLE JSON_CITY_INGEST;
-DROP TABLE CITY;
-DROP TABLE GEOGRAPHY;
 
 
-CREATE TABLE JSON_CITY_INGEST(v1 VARIANT);
+CREATE OR REPLACE  TABLE JSON_CITY_INGEST(v1 VARIANT);
+CREATE OR REPLACE  TABLE JSON_CITYTEMP_INGEST(v1 VARIANT);
+
+CREATE OR REPLACE STREAM my_citystream ON TABLE JSON_CITY_INGEST;
+CREATE OR REPLACE STREAM my_citystream ON TABLE JSON_CITYTEMP_INGEST;
 --list @AZURE_KP;
 --Select v1 from @AZURE_KP/OW.City.ndjson.gz;
 COPY INTO JSON_CITY_INGEST
 FROM @AZURE_KP/OW.City.ndjson.gz file_format = (TYPE = json);
 
+COPY INTO JSON_CITYTEMP_INGEST
+FROM @AZURE_KP/climate.json file_format = JSON;
+
+
 --SELECT *
 --FROM JSON_CITY_INGEST
 --LIMIT 10;
 
-CREATE TABLE CITY (id INT, NAME VARCHAR(50), country VARCHAR(2));
-CREATE TABLE GEOGRAPHY(city_id INT, lon FLOAT, lat FLOAT);
+CREATE OR REPLACE TABLE CITY (id INT, NAME VARCHAR(50), country VARCHAR(2));
+CREATE OR REPLACE TABLE GEOGRAPHY(city_id INT, lon FLOAT, lat FLOAT);
+
+
+create or replace table city_population  
+(	
+	Country string,
+  	City string,
+  	AccentCity string,
+  	Region integer,
+  	Population float,
+  	latitude float,
+  	longitude float
+);
+ 
+create or replace table city_temp  
+(	
+	Region string,
+	Country string,
+	State string,
+	City string,
+	MONTH integer,
+	DAY integer,
+	YEAR integer,
+	AvgTemperature float	
+);
+
+COPY INTO city_population
+FROM @AZURE_KP/2015_3689_compressed_worldcitiespop.csv.zip file_format =  CSV2;
+
+COPY INTO city_temp
+FROM @AZURE_KP/694560_1215964_compressed_city_temperature.csv.zip file_format =  CSV2;
+
+Select *, 
+FROM "CITYBIKE"."PUBLIC"."JSON_CITY_INGEST" limit 100
--- SnowFlake/LoadDataFromLocalIngestTableMerge.sql
+++ SnowFlake/LoadDataFromLocalIngestTableMerge.sql
@@ -1,34 +1,65 @@
 
 USE WAREHOUSE COMPUTE_WH;
-USE DATABASE CITIBIKE;
+USE DATABASE CITYBIKE;
 
 
-SELECT
+/*SELECT
 	v1:id::INT AS city_id,
 	v1:NAME::VARCHAR(50) AS city_name,
 	v1:country::VARCHAR(2) AS country,
 	v1:coord.lat::FLOAT AS city_lat,
 	v1:coord.lon::FLOAT AS city_lon
 FROM
-	JSON_CITY_INGEST;
+	JSON_CITY_INGEST
+WHERE city_id>150000 AND city_id<251332
+ORDER BY city_id;
+*/
+--- MERGE INTO CITY ---
 
+MERGE INTO CITY
+	USING (	
+	SELECT
+		v1:id::INT AS city_id,
+		v1:NAME::VARCHAR(50) AS city_name,
+		v1:country::VARCHAR(2) AS country 
+	FROM JSON_CITY_INGEST 
+	WHERE city_id>150000 AND city_id<251332
+	) AS src 
+	ON	CITY.id = src.city_id	
+	WHEN MATCHED THEN
+		UPDATE
+		SET
+			CITY.name = src.city_name,
+			CITY.country = src.country		
+	/*WHEN NOT MATCHED and src.city_id is null THEN DELETE*/
+	WHEN NOT MATCHED THEN
+		INSERT (id, name, country)
+		VALUES (src.city_id, src.city_name, src.country);
 
+--- MERGE INTO GEOGRAPHY ---
 MERGE INTO GEOGRAPHY
 	USING (
 	SELECT
 		v1:id::INT AS city_id,
 		v1:coord.lat::FLOAT AS city_lat,
 		v1:coord.lon::FLOAT AS city_lon
-	FROM JSON_CITY_INGEST	LIMIT 10) AS src 
-	ON	GEOGRAPHY.city_id = src.city_id
+	FROM JSON_CITY_INGEST 
+	WHERE city_id>150000 AND city_id<251332
+	) AS src 
+	ON	GEOGRAPHY.city_id = src.city_id	
 	WHEN MATCHED THEN
 		UPDATE
 		SET
 			GEOGRAPHY.lat = src.city_lat,
-			GEOGRAPHY.lon = src.city_lon			
+			GEOGRAPHY.lon = src.city_lon		
+	/*WHEN NOT MATCHED and src.city_id is null THEN DELETE*/
 	WHEN NOT MATCHED THEN
 		INSERT (city_id, lat, lon)
 		VALUES (src.city_id, src.city_lat, src.city_lon);
 
+	
+/*SELECT	*
+FROM	CITY;	
+	
 SELECT	*
-FROM	GEOGRAPHY;
+FROM	GEOGRAPHY;*/
